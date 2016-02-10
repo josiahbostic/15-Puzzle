8d0023d8250b13504d28077981084859b8731e88
Earl Fong <earl_fong@operamail.com>
2016-02-10 14:30:43 -0500

    Fixed indentation, replaced tabs.

--- 15-puzzle.js
+++ 15-puzzle.js
@@ -1,105 +1,105 @@
 
 var board = {
 
-	// For each array element, the value is the number tile currently in that slot.
-	// The value 0 represents the blank slot.
-	state: [ null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 ],
-	completedState: [ null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 ],
-	blankSlot: 16,  // Index of where the blank is, ie. the 0 value.
+  // For each array element, the value is the number tile currently in that slot.
+  // The value 0 represents the blank slot.
+  state: [ null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 ],
+  completedState: [ null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 ],
+  blankSlot: 16,  // Index of where the blank is, ie. the 0 value.
 
-	// For each array element, the value is a subarray that lists the adjacent slots.
-	adjacent: [null, 
-					 [2,5],   	[1,3,6], 		[2,4,7], 		[3,8],
-			         [1,6,9], 	[2,5,7,10], 	[3,6,8,11], 	[4,7,12],
-			         [5,10,13], [6,9,11,14], 	[7,10,12,15], 	[8,11,16],
-			         [9,14], 	[10,13,15], 	[11,14,16], 	[12,15]
-			  ],
+  // For each array element, the value is a subarray that lists the adjacent slots.
+  adjacent: [null,
+               [2,5],     [1,3,6],     [2,4,7],      [3,8],
+               [1,6,9],   [2,5,7,10],  [3,6,8,11],   [4,7,12],
+               [5,10,13], [6,9,11,14], [7,10,12,15], [8,11,16],
+               [9,14],    [10,13,15],  [11,14,16],   [12,15]
+            ],
 
 
-	renderTiles: function () {
-		$('td').each ( 
-			function renderOneSlot () {
-				var id = $(this).attr('id');
-				var idNum = id.substring(5);  // Strip off the leading "slot-" to extract just the number.
-				var tile = board.state[idNum];
-				if (tile === 0) {  // It's the blank slot.
-					$(this).addClass('blank');
-					$(this).text('');
-				}
-				else {
-					$(this).removeClass('blank');
-				  	$(this).text(tile);			  	
-				}
-			}
-		);
+  renderTiles: function () {
+    $('td').each (
+      function renderOneSlot () {
+        var id = $(this).attr('id');
+        var idNum = id.substring(5);  // Strip off the leading "slot-" to extract just the number.
+        var tile = board.state[idNum];
+        if (tile === 0) {  // It's the blank slot.
+          $(this).addClass('blank');
+          $(this).text('');
+        }
+        else {
+          $(this).removeClass('blank');
+            $(this).text(tile);
+        }
+      }
+    );
 
-		board.makeSlotsClickable(board.blankSlot);
-	},
+    board.makeSlotsClickable(board.blankSlot);
+  },
 
-	makeSlotsClickable: function(idNum){
+  makeSlotsClickable: function(idNum){
 
-		var movableSlots = board.adjacent[idNum];
-		var selector = "";
-		for(var i = 0; i < movableSlots.length; i++){
-			selector += ", #slot-" + movableSlots[i];
-		}
-		selector = selector.substring(2);  // Strip off the leading ", ".
-		$(selector).wrapInner('<a href="#"></a>');
-	},
+    var movableSlots = board.adjacent[idNum];
+    var selector = "";
+    for(var i = 0; i < movableSlots.length; i++){
+      selector += ", #slot-" + movableSlots[i];
+    }
+    selector = selector.substring(2);  // Strip off the leading ", ".
+    $(selector).wrapInner('<a href="#"></a>');
+  },
 
-	makeMove: function(tile){
+  makeMove: function(tile){
 
-				var idNum = $(tile).parent().attr('id').substring(5);
-				var tileValue = board.state[idNum];
-				board.state[idNum] = 0;
-				board.state[board.blankSlot] = tileValue;
-				board.blankSlot = idNum;
+    var idNum = $(tile).parent().attr('id').substring(5);
+    var tileValue = board.state[idNum];
+    board.state[idNum] = 0;
+    board.state[board.blankSlot] = tileValue;
+    board.blankSlot = idNum;
 
-				board.renderTiles();
-	},
+    board.renderTiles();
+  },
 
-	setSlotHandler: function(){
+  setSlotHandler: function(){
 
-		$('td').on('click', 'a', 
-			function(){
-				board.makeMove(this); 
-				if (board.isGameFinished()) board.userWon();
-			}
-		);
-	},
+    $('td').on('click', 'a',
+      function(){
+        board.makeMove(this);
+        if (board.isGameFinished()) board.userWon();
+      }
+    );
+  },
 
-	isGameFinished: function(){
+  isGameFinished: function(){
 
-		for (var i = 0; i < board.state.length; i++){
+    for (var i = 0; i < board.state.length; i++){
 
-			if(board.state[i] != board.completedState[i]) return false;
-		}
+      if(board.state[i] != board.completedState[i]) return false;
+    }
 
-		return true;
-	},
+    return true;
+  },
 
-	userWon: function(){
+  userWon: function(){
 
-		alert('You win!');
-	},
+    alert('You win!');
+  },
 
-	setShuffleHandler: function () {
+  setShuffleHandler: function () {
 
-		$('#shuffle-board').on('click',
+    $('#shuffle-board').on('click',
 
-			function shuffleTheBoard () {
+      function shuffleTheBoard () {
 
-				// Initialize the shuffled board to the completed state,
-				// then make random moves to do the shuffling.
-				var shuffledState = board.completedState.slice(0);
-				var movableTiles = board.adjacent[board.blankSlot];
-				var whichTile = Math.floor(Math.random() * movableTiles.length);
-				var tileIdNum = 'slot-' + movableTiles[whichTile];
+        // Initialize the shuffled board to the completed state,
+        // then make random moves to do the shuffling.
+        var shuffledState = board.completedState.slice(0);
+        var movableTiles = board.adjacent[board.blankSlot];
+        var whichTile = Math.floor(Math.random() * movableTiles.length);
+        var tileIdNum = 'slot-' + movableTiles[whichTile];
 
-				alert(tileIdNum);
-			}
-		)
-	}
+        alert(tileIdNum);
+      }
+    )
+  }
 };
 
 
